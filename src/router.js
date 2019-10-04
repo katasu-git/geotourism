171e419904f85b9c7296e79c2b7c652d146e5b0f
katasu-git <katasu9923@gmail.com>
2019-10-04 13:11:48 +0900

    vue-routerを導入してtopページのファイルを作った

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Top from "./views/Top";
+
+
+Vue.use(VueRouter);
+const routes = [
+    {
+        path: "/",
+        name: "top",
+        component: Top
+    }
+];
+
+export default new VueRouter({
+    routes: routes
+})
---
+++ src/views/Top.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="top">
+      topPage!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'top',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    goVotePage: function() {
+      setTimeout(() => {
+          this.$router.push({
+          name: 'vote',
+          params: {
+            routerPeopleNum: this.peopleNum,
+          }
+        })
+      }, 300);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
